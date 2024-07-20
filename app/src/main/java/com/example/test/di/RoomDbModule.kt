dd1dc31e8a019870932331753ad7c9fd627f4d60
shamil <shahmardanov.shamil@gmailcom>
2024-07-20 13:14:22 +0400

    add methods to roomdbmodule lass

---
+++ app/src/main/java/com/example/test/di/RoomDbModule.kt
@@ -0,0 +1,29 @@
+package com.example.test.di
+
+import android.content.Context
+import androidx.room.Room
+import com.example.test.local.CoffeeShopDB
+import com.example.test.local.CoffeeShopDao
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RoomDbModule {
+
+    @Provides
+    @Singleton
+    fun provideRoomDb(@ApplicationContext context: Context): CoffeeShopDB {
+        return Room.databaseBuilder(context, CoffeeShopDB::class.java, "CoffeeShopDB").build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideDataBase(coffeeShopDB: CoffeeShopDB): CoffeeShopDao {
+        return coffeeShopDB.productDao()
+    }
+}
--- app/src/main/java/com/example/test/local/CoffeeShopDB.kt
+++ app/src/main/java/com/example/test/local/CoffeeShopDB.kt
@@ -2,9 +2,10 @@ package com.example.test.local
 
 import ProductEntity
 import androidx.room.Database
+import androidx.room.RoomDatabase
 
 @Database(entities = [ProductEntity::class], version = 1)
-abstract class CoffeeShopDB {
+abstract class CoffeeShopDB :RoomDatabase(){
 
     abstract fun productDao(): CoffeeShopDao
 }
