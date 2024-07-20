40b79bb139808a1cebe22c67dd81ea515d24589d
shamil <shahmardanov.shamil@gmailcom>
2024-07-20 17:50:01 +0400

    add register fragment and xml file class

--- app/src/main/java/com/example/test/splash/SplashFragment.kt
+++ app/src/main/java/com/example/test/splash/SplashFragment.kt
@@ -5,11 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import com.example.test.R
 import com.example.test.base.BaseFragment
 import com.example.test.databinding.FragmentSplashBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 
 class SplashFragment : BaseFragment<FragmentSplashBinding>(FragmentSplashBinding::inflate) {
 
+   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+       super.onViewCreated(view, savedInstanceState)
+       navigateToHome()
+
+   }
+
+    private fun navigateToHome() {
+        lifecycleScope.launch {
+            delay(1500)
+            findNavController().navigate(SplashFragmentDirections.actionSplashFragmentToRegisterFragment())
+        }
+    }
 }
