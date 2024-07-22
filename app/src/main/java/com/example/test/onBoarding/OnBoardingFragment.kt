44e69b85cae37b2f55f9537006c21fec48b64d13
shamil <shahmardanov.shamil@gmailcom>
2024-07-22 15:16:19 +0400

    add onboarding fragment

---
+++ app/src/main/java/com/example/test/onBoarding/OnBoardingFragment.kt
@@ -0,0 +1,18 @@
+package com.example.test.onBoarding
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.test.R
+import com.example.test.base.BaseFragment
+import com.example.test.databinding.FragmentOnBoardingBinding
+
+
+class OnBoardingFragment : BaseFragment<FragmentOnBoardingBinding>(FragmentOnBoardingBinding::inflate) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
