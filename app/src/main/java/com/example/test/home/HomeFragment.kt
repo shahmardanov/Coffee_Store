d73f6ce1ee69bd775025eee663d303ea75c8cb7d
shamil <shahmardanov.shamil@gmailcom>
2024-07-23 20:27:54 +0400

    home fragment ui is done

---
+++ app/src/main/java/com/example/test/home/HomeFragment.kt
@@ -0,0 +1,19 @@
+package com.example.test.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.test.R
+import com.example.test.base.BaseFragment
+import com.example.test.databinding.FragmentHomeBinding
+
+
+class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
---
+++ app/src/main/java/com/example/test/home/HomeViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.test.home
+
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+}
